619b3efb6f3b5a759423d92ac7af04a8106f2947
rafalboguski <BoguskiRafal@gmail.com>
2016-02-28 23:49:17 +0100

    Editor: using AvalonEdit library for editor panel

--- SQL/SQL/MainWindow.xaml.cs
+++ SQL/SQL/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MahApps.Metro.Controls;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.TextEditor.Document;
+using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -19,6 +22,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace SQL
 {
@@ -27,7 +31,16 @@ namespace SQL
         public MainWindow()
         {
             InitializeComponent();
-            Editor.Focus();
+            //Editor.Focus();
+
+            using (var s = new StreamReader(@"C:\Users\user\Documents\GitHubVisualStudio\Formal-Language-Compilers\SQL\SQL\Resources\Colorizer.xshd"))
+            {
+                var line = s.ReadLine();
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    textEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+                }
+            }
         }
 
         private void Colorize_Key(object sender, KeyEventArgs e)
@@ -43,7 +56,7 @@ namespace SQL
 
         private void Colorize()
         {
-            Editor.HighlightSyntax();
+            //Editor.HighlightSyntax();
         }
     }
 
