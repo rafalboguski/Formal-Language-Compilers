d0c488ef36462b1f781e1213d2178e4ad977e9e5
rafalboguski <BoguskiRafal@gmail.com>
2016-03-02 20:45:10 +0100

    Editor: UI changes

---
+++ SQL/SQL/Extensions/ControllsWpf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace SQL.Extensions
+{
+    static class ControllsWpf
+    {
+        public static string GetText(this RichTextBox richTextBox)
+        {
+            return new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+        }
+    }
+}
--- SQL/SQL/MainWindow.xaml.cs
+++ SQL/SQL/MainWindow.xaml.cs
@@ -31,138 +31,53 @@ namespace SQL
         public MainWindow()
         {
             InitializeComponent();
-            //Editor.Focus();
+            Editor.Focus();
 
+            Editor.ShowLineNumbers = true;
+            Editor.Text = "select * from t_user where id = '34';\n\n\n";
+
+            // load syntax theme
             using (var s = new StreamReader(@"C:\Users\user\Documents\GitHubVisualStudio\Formal-Language-Compilers\SQL\SQL\Resources\Colorizer.xshd"))
             {
                 var line = s.ReadLine();
                 using (XmlTextReader reader = new XmlTextReader(s))
                 {
-                    textEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+                    Editor.SyntaxHighlighting = HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
+                }
+            }
+
+            Console.ShowLineNumbers = true;
+            // load syntax theme
+            using (var s = new StreamReader(@"C:\Users\user\Documents\GitHubVisualStudio\Formal-Language-Compilers\SQL\SQL\Resources\Colorizer.xshd"))
+            {
+                var line = s.ReadLine();
+                using (XmlTextReader reader = new XmlTextReader(s))
+                {
+                    Console.SyntaxHighlighting = HighlightingLoader.Load(reader, ICSharpCode.AvalonEdit.Highlighting.HighlightingManager.Instance);
                 }
             }
         }
 
         private void Colorize_Key(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.F5)
-                Colorize();
+            //if (e.Key == Key.F5)
         }
 
         private void Colorize_Button_Click(object sender, RoutedEventArgs e)
         {
-            Colorize();
         }
 
-        private void Colorize()
+        private void Editor_KeyDown(object sender, KeyEventArgs e)
         {
-            //Editor.HighlightSyntax();
+            Console.Text = "";
+
+            var list = Editor.Text.ToString().Split().Distinct().ToList();
+            foreach (var x in list)
+            {
+                float hash = x.GetHashCode() / 10000000f;
+                Console.Text += (Math.Pow(hash, 2)).ToString() + " ";
+            }
+
         }
     }
-
-
-    public static class RichTextBoxExtensions
-    {
-
-        public static string GetText(this RichTextBox richTextBox)
-        {
-            return new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
-        }
-
-        private static List<string> _keywords = new List<string>
-        {
-          "select","from","where","update","delete","alter",
-          "and","or","order by" ," desc"," asc","insert","into","values" ,"join", "as ","like ","not ","on " ,
-            ",", "\"", "\'", ";", "=", "-", "+" ,"(", ")"
-
-        };
-
-        public static List<string> _signs = new List<string>
-        {
-        };
-
-        public static void HighlightSyntax(this RichTextBox richTextBox)
-        {
-            var text = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text.ToString().ToLower();
-            var textRange = richTextBox.Selection;
-
-            var savedCaretPosition = richTextBox.CaretPosition;
-
-            //clear colors
-            textRange.Select(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
-            textRange.ApplyPropertyValue(TextElement.ForegroundProperty, new SolidColorBrush(Colors.White));
-
-            ColorizeSet(richTextBox, text, _keywords, new SolidColorBrush(Colors.Teal));
-            ColorizeSet(richTextBox, text, _signs, new SolidColorBrush(Colors.Yellow));
-
-            richTextBox.CaretPosition = savedCaretPosition;
-        }
-
-        private static void ColorizeSet(RichTextBox richTextBox, string text, List<string> keywords, SolidColorBrush color)
-        {
-            foreach (var keyword in keywords)
-            {
-                var indexes = text.AllIndexesOf(keyword).ToArray();
-                TextPointer startPos = richTextBox.Document.ContentStart; ;
-                for (var i = 0; i < indexes.Count(); i++)
-                {
-                    if (i > 0)
-                    {
-                        startPos = GetPoint(startPos, indexes[i] - indexes[i - 1]);
-                    }
-                    else
-                    {
-                        startPos = GetPoint(startPos, indexes[i]);
-                    }
-                    Colorize(richTextBox, startPos, indexes[i], keyword.Length, color);
-                }
-            }
-        }
-
-
-        private static void Colorize(RichTextBox richTextBox, TextPointer from, int offset, int length, SolidColorBrush color)
-        {
-            var startPos = from;
-            var endPos = GetPoint(startPos, length);
-
-            richTextBox.Selection.Select(startPos, endPos);
-            richTextBox.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, color);
-        }
-
-        private static TextPointer GetPoint(TextPointer start, int x)
-        {
-            var begin = start;
-            var i = x;
-            while (begin != null)
-            {
-                var xxx = new TextRange(begin, begin.GetPositionAtOffset(i, LogicalDirection.Forward)).Text.Length;
-                if (xxx == x)
-                    break;
-                i++;
-                if (begin.GetPositionAtOffset(i, LogicalDirection.Forward) == null)
-                    return begin.GetPositionAtOffset(i - 1, LogicalDirection.Forward);
-            }
-            begin = begin.GetPositionAtOffset(i, LogicalDirection.Forward);
-            return begin;
-        }
-
-    }
-
-    public static class StandardExtensions
-    {
-        public static List<int> AllIndexesOf(this string str, string value)
-        {
-            if (String.IsNullOrEmpty(value))
-                throw new ArgumentException("the string to find may not be empty", "value");
-            List<int> indexes = new List<int>();
-            for (int index = 0; ; index += value.Length)
-            {
-                index = str.IndexOf(value, index);
-                if (index == -1)
-                    return indexes.OrderBy(x => x).ToList();
-                indexes.Add(index);
-            }
-        }
-    }
-
 }
