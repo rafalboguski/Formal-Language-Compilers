2d1e450a0ffef97c3f28bc077b43b1de4b37632f
rafalboguski <BoguskiRafal@gmail.com>
2016-03-18 20:00:58 +0100

    Parser: prototype of tokenizer

--- SQL/SQL_Parser/ParseSql.cs
+++ SQL/SQL_Parser/ParseSql.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using SQL_Parser.Statements;
 
 namespace SQL_Parser
 {
@@ -12,31 +11,127 @@ namespace SQL_Parser
     class ParserSql
     {
 
-        private List<Word> _grammar;
+
+        private List<Keyword> _keywords = new List<Keyword>()
+        {
+            new Keyword("select", "(SELECT|select)"),
+            new Keyword("from", "(FROM|from)"),
+            new Keyword("where", "(WHERE|where)"),
+            new Keyword("space", @"(\s+)"),
+            new Keyword("semicolon", "(;)")
+        };
 
 
         public ParserSql()
         {
-            _grammar = new List<Word>();
+            var INPUT = "select NAME from   NAME;";
+            Console.WriteLine(INPUT);
+            var keywordMatches = new List<Token>();
 
-           // _grammar.Add(new Word_SELECT());
+            // tokenize keywords
+            foreach (var keyword in _keywords)
+            {
+                var reg = new Regex(keyword.Regex);
+                var matches = reg.Matches(INPUT);
+
+                foreach (Match match in matches)
+                {
+                    keywordMatches.Add(new Token(keyword.Name, match));
+                }
+
+                var t = 234;
 
 
-            var SEL = new Word_SELECT();
+            }
+            keywordMatches = keywordMatches.OrderBy(x => x.Index).ToList();
+            Console.WriteLine("keywords");
+            foreach (var token in keywordMatches)
+            {
+                Console.Write("<" + token.Name + "> ");
+            }
 
-            var r = SEL.Regeex;
+            // tokenize rest
+            var tokens = new List<Token>(keywordMatches);
 
-            var boo = SEL.IsMatch("select;");
-            var boo2 = SEL.IsMatch("select ;");
-            var boo3 = SEL.IsMatch("SELECT;");
+            for (int i = 0; i < keywordMatches.Count() - 1; i++)
+            {
+                var array = keywordMatches.ToArray();
+                var token = array[i];
+                var nextToken = array[i + 1];
 
-              
 
-            var sdfsdf = 4;
+
+                if (nextToken.Index != token.Index + token.Length)
+                {
+                    var index = token.Index + token.Length;
+                    var length = nextToken.Index - (token.Index + token.Length);
+                    var text = INPUT.Substring(index, length);
+                    tokens.Add(new Token(text, index, length));
+                }
+
+            }
+
+            tokens = tokens.OrderBy(x => x.Index).ToList();
+
+
+
+
+            Console.WriteLine("\nall");
+
+            foreach (var token in tokens)
+            {
+                Console.Write("<" + token.Name + "> ");
+            }
+
+            // validate
+
+
+
+            var OUTPUT = "";
+            Console.Read();
+        }
+
+
+
+    }
+
+    class Keyword
+    {
+
+        public string Name { get; set; }
+        public string Regex { get; set; }
+
+        public Keyword(string name, string regex)
+        {
+            this.Name = name;
+            this.Regex = regex;
         }
 
     }
 
-    
+    class Token
+    {
+
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+        public int Length { get; set; }
+
+
+
+        public Token(string name, Match match)
+        {
+            Name = name;
+            Index = match.Index;
+            Length = match.Length;
+        }
+        public Token(string name, int index, int length)
+        {
+            Name = name;
+            Index = index;
+            Length = length;
+        }
+
+    }
 
 }
