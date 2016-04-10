100de9ab954f298976dc2c971017437a9dc79ed1
rafalboguski <boguskirafal@gmail.com>
2016-04-10 22:11:22 +0200

    DAS Refactor

--- SQL/Regex DAS/DAS.cs
+++ SQL/Regex DAS/DAS.cs
@@ -12,18 +12,20 @@ namespace Regex_DAS
     {
         private State CurrentState;
         private List<State> StatesTable;
+        private List<string> Keys;
+        private string temporaryFoundElement = "";
         private List<string> FoundElements;
 
-        public DAS()
+        public DAS(List<string> keys = null, List<State> moveTable = null)
         {
-            StatesTable = new List<State>()
+            StatesTable = moveTable == null ? new List<State>()
             {
-                new State(0,  new Dictionary<string, int>(){{ "[A-Z]", 1}}),
-                new State(1,  new Dictionary<string, int>(){{ "[A-Z]", 2}}),
-                new State(2,  new Dictionary<string, int>(){{ " ", 3}, { "[0-9]", 4}}),
-                new State(3,  new Dictionary<string, int>(){{ "[0-9]", 4}}),
-                new State(4,  new Dictionary<string, int>(){{ "[A-Z]", 8}, { "[0-9]", 5}}),
-                new State(5,  new Dictionary<string, int>(){{ "[0-9]", 6} }),
+                new State(0,  new Dictionary<string, int>(){{ "[A-Z]", 1 } }),
+                new State(1,  new Dictionary<string, int>(){{ "[A-Z]", 2 } }),
+                new State(2,  new Dictionary<string, int>(){{ "[0-9]", 4 }, { " ", 3}}),
+                new State(3,  new Dictionary<string, int>(){{ "[0-9]", 4 } }),
+                new State(4,  new Dictionary<string, int>(){{ "[A-Z]", 8 }, { "[0-9]", 5}}),
+                new State(5,  new Dictionary<string, int>(){{ "[0-9]", 6 } }),
                 new State(6,  new Dictionary<string, int>(){{ "[A-Z]", 11}, { "[0-9]", 7 } }),
                 new State(7,  new Dictionary<string, int>(){{ "[A-Z]", 12}, { "[0-9]", 12 } }),
                 new State(8,  new Dictionary<string, int>(){{ "[A-Z]", 9 }, { "[0-9]", 9 } }),
@@ -31,15 +33,18 @@ namespace Regex_DAS
                 new State(10, new Dictionary<string, int>(){{ "[0-9]", 12} }),
                 new State(11, new Dictionary<string, int>(){{ "[A-Z]", 12} }),
                 new State(12, new Dictionary<string, int>(){}),
+            } : moveTable;
 
-
-            };
+            Keys = keys == null ? new List<string>()
+            {
+                "[A-Z]",
+                "[0-9]",
+                " "
+            } : keys;
 
             CurrentState = StatesTable.First();
             FoundElements = new List<string>();
         }
-
-        private string temporaryFoundElement = "";
 
         public List<string> Start(string input)
         {
@@ -52,17 +57,10 @@ namespace Regex_DAS
 
         private void ChangeState(char inKey)
         {
-            var keys = new List<string>()
-            {
-                "[A-Z]",
-                "[0-9]",
-                " "
-            };
-
             if (CurrentState.Number == StatesTable.First().Number)
                 temporaryFoundElement = "";
 
-            foreach (var key in keys)
+            foreach (var key in Keys)
             {
                 if (Regex.IsMatch(inKey.ToString(), $@"^{key}") && CurrentState.MovesTable.ContainsKey(key.ToString()))
                 {
@@ -85,15 +83,10 @@ namespace Regex_DAS
         public int Number;
         public Dictionary<string, int> MovesTable;
 
-        public State(int number, Dictionary<string, int> moves)
+        public State(int stateNumber, Dictionary<string, int> movesTable)
         {
-            Number = number;
-            MovesTable = moves;
-        }
-
-        public override string ToString()
-        {
-            return $"State {Number}";
+            Number = stateNumber;
+            MovesTable = movesTable;
         }
     }
 }
